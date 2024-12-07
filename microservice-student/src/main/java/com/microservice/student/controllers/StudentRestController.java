d6b0b41cc788924770014226cf46e62cb90243d1
david.torrez <david.torrez@micrium.com.bo>
2024-12-07 18:42:43 -0400

    Add configuracion Config Server y ApiGateway

--- microservice-student/src/main/java/com/microservice/student/controllers/StudentRestController.java
+++ microservice-student/src/main/java/com/microservice/student/controllers/StudentRestController.java
@@ -13,8 +13,6 @@ import com.microservice.student.services.IStudentService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 
 @RestController
