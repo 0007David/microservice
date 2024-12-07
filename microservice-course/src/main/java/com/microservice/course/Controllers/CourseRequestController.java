257088fd3244219d0846d7b722638d41c0f31821
david.torrez <david.torrez@micrium.com.bo>
2024-12-07 06:45:55 -0400

    add Implementacion de mscv Controller Course

--- microservice-course/src/main/java/com/microservice/course/Controllers/CourseRequestController.java
+++ microservice-course/src/main/java/com/microservice/course/Controllers/CourseRequestController.java
@@ -1,5 +1,41 @@
 package com.microservice.course.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservice.course.models.Course;
+import com.microservice.course.services.ICourseService;
+
+@RestController
+@RequestMapping("/api/course")
 public class CourseRequestController {
+
+    @Autowired
+    private ICourseService courseService;
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveCourse(@RequestBody Course student) {
+        courseService.save(student);
+    }
+    
+    @GetMapping("/all")
+    public ResponseEntity<?> findAllCourse(@RequestParam String param) {
+        return ResponseEntity.ok(courseService.findAll());
+    }
+    
+    @GetMapping("/search/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(courseService.findById(id));
+    }
 
 }
